a087186dbd98dc26d813b6ea025205f3b2f33e9b
Kie6er <kirill.zawgorodny@yandex.ru>
2023-11-17 18:50:42 +0300

    feat: main-page

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import "./index.scss";
 import "./js/pages/home";
 import "./js/components/header";
 import { example } from "./js/utils/constants";
+import "./js/utils/swiper";
+import 'swiper/css';
+import $ from 'jquery'
