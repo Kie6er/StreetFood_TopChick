54e38b95d5ee4100ed861327a27c56ab7c8f8feb
Kie6er <kirill.zawgorodny@yandex.ru>
2023-11-24 16:36:35 +0300

    fix

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,10 +1,12 @@
 import $ from 'jquery';
 $(document).ready(function () {
 	if (window.outerWidth <= 768) {
+		setTimeout(function () {
+			$('.burger-menu').css('display', 'flex');
+		}, 0)
 		$('.burger-btn').on('click', function () {
 			$(this).toggleClass('active');
 			$('.burger-menu').toggleClass('show');
-			$('.burger-menu').css('display', 'flex');
 			$('body').toggleClass('lock');
 		})
 
@@ -12,19 +14,16 @@ $(document).ready(function () {
 			$('.burger-btn').removeClass('active');
 			$('.burger-menu').removeClass('show');
 			$('body').removeClass('lock');
-
-			if (window.outerWidth <= 768) {
-				$('.burger-menu').css('display', 'flex');
-			} else {
-				$('.burger-menu').css('display', 'none');
-			}
 		})
-	}
-	if (window.outerWidth <= 768) {
-		setTimeout(function () {
-			$('.burger-menu').css('display', 'flex');
-		}, 0)
 	} else {
 		$('.burger-menu').css('display', 'none');
 	}
+
+	$(window).resize(function () {
+		if (window.outerWidth <= 768) {
+			$('.burger-menu').css('display', 'flex');
+		} else {
+			$('.burger-menu').css('display', 'none');
+		}
+	})
 })
