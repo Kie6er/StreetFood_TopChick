6cc2da21909cd9b12bf4e5c2fba0f3b76fdc3436
Kie6er <kirill.zawgorodny@yandex.ru>
2023-12-13 12:04:08 +0300

    fix merge

--- src/js/utils/map.js
+++ src/js/utils/map.js
@@ -5,7 +5,6 @@ import {
 	Pagination,
 } from "swiper/modules";
 
-(function () {
 const markers = [
 	{
 		address: 'Можайский вал, 10',
@@ -36,14 +35,6 @@ const markers = [
 		sliderIndex: 3,
 	},
 ];
-const mapModalMarkers = [
-	{
-		address: 'Можайский вал, 10',
-		coordinate: [37.528583852499565, 55.73236309372132],
-		active: true,
-		type: 'fast-food',
-	}
-];
 const mapSlider = new Swiper('.main-map__slider', {
 	modules: [Navigation, Pagination],
 	direction: 'horizontal',
@@ -69,9 +60,6 @@ const mapSlider = new Swiper('.main-map__slider', {
 });
 $(document).ready(function () {
 	$('.main-map').length > 0 ? initMap() : null;
-	// $('[data-modal="map"]').on('click', () => {
-	// 	$('.map-modal').length > 0 ? initModalMap() : null;
-	// });
 });
 function remToPx(remValue) {
 	// Получаем текущий базовый размер шрифта (font-size) из элемента <html>
@@ -81,20 +69,21 @@ function remToPx(remValue) {
 
 	// Переводим значение из rem в px
 	var pxValue = remValue * htmlFontSize;
-		// Округляем значение до целых пикселей (по желанию)
-		return Math.round(pxValue) + "px";
-	}
-	async function initMap() {
-		await ymaps3.ready;
-		const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker } = ymaps3;
-		const map = new YMap(document.getElementById('map-main'), {
-			location: {
-				center: [37.588144, 55.733842],
-				zoom: 12
-			},
-		});
-		map.addChild(new YMapDefaultSchemeLayer());
-		map.addChild(new YMapDefaultFeaturesLayer({ zIndex: 1800 }));
+
+	// Округляем значение до целых пикселей (по желанию)
+	return Math.round(pxValue) + "px";
+};
+async function initMap() {
+	await ymaps3.ready;
+	const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker } = ymaps3;
+	const map = new YMap(document.getElementById('map-main'), {
+		location: {
+			center: [37.588144, 55.733842],
+			zoom: 12
+		},
+	});
+	map.addChild(new YMapDefaultSchemeLayer());
+	map.addChild(new YMapDefaultFeaturesLayer({ zIndex: 1800 }));
 
 	markers.forEach((el) => {
 		let content = document.createElement('div');
@@ -105,28 +94,29 @@ function remToPx(remValue) {
 					<path d="M27.5682 12.134C27.9958 12.4123 29.4087 13.0802 30.617 12.5607C32.0299 11.967 31.5094 10.4643 30.2638 9.51819L32.662 9.25846L30.5799 6.8096C31.9556 3.85984 29.9292 2.46844 28.8881 2.17161C28.1817 1.96754 27.2522 2.00464 26.49 2.50555C25.9323 0.92863 24.7425 -0.0360707 23.2553 0.00103315C18.9051 0.112345 20.0391 4.93585 20.0391 4.93585L25.3002 5.47386C25.3002 5.47386 27.3823 5.64083 26.6945 5.67793C21.2475 6.08607 18.1057 12.6349 18.1057 18.7014L23.9803 39.4239L29.7247 35.0271C35.2089 29.1832 33.3312 23.3394 30.0779 16.939C28.7394 14.3973 27.7913 12.5422 27.5682 12.134Z" fill="#FFF4DA"/>
 				</svg>
 			`);
-			el.active ? $(content).addClass('active') : null;
-			clickMarker(content, el);
-			const marker = new YMapMarker({ coordinates: el.coordinate, draggable: false }, content);
-			map.addChild(marker);
-		})
-	}
-	function clickMarker(content, marker) {
-		$(content).on('click', function () {
-			changeMarkers(marker, content);
-			mapSlider.slideTo(marker.sliderIndex, 800);
-		});
+		el.active ? $(content).addClass('active') : null;
+		clickMarker(content, el);
+		const marker = new YMapMarker({ coordinates: el.coordinate, draggable: false }, content);
+		map.addChild(marker);
+	})
+};
 
-		$('.main-map__navigation-btn').on('click', function () {
-			if (mapSlider.activeIndex === marker.sliderIndex) {
-				changeMarkers(marker, content);
-			};
-		});
-	};
-	function changeMarkers(marker, content) {
-		$('.marker').removeClass('active');
-		markers.forEach(el => el.active = false);
-		marker.active = true;
-		$(content).addClass('active')
-	}
-}())
+function clickMarker(content, marker) {
+	$(content).on('click', function () {
+		changeMarkers(content, marker);
+		mapSlider.slideTo(marker.sliderIndex, 800);
+	});
+
+	$('.main-map__navigation-btn').on('click', function () {
+		if (mapSlider.activeIndex === marker.sliderIndex) {
+			changeMarkers(content, marker);
+		};
+	});
+};
+
+function changeMarkers(content, marker) {
+	$('.marker').removeClass('active');
+	markers.forEach(el => el.active = false);
+	marker.active = true;
+	$(content).addClass('active')
+};
